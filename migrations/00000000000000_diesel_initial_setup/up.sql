1f4a30fd6d748233a13fc68a16844b2f12c5a524
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2026-03-28 16:33:27 -0400

    fix: enable uuid-ossp extension in initial migration

--- migrations/00000000000000_diesel_initial_setup/up.sql
+++ migrations/00000000000000_diesel_initial_setup/up.sql
@@ -2,8 +2,8 @@
 -- and other internal bookkeeping. This file is safe to edit, any future
 -- changes will be added to existing projects as new migrations.
 
-
-
+-- Enable UUID generation functions
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Sets up a trigger for the given table to automatically set a column called
 -- `updated_at` whenever the row is modified (unless `updated_at` was included
